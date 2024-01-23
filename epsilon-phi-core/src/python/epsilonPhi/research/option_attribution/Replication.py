3199698dff3898ba0ebb92bcd47d24198be83c44
Epsilon Phi <123123634+fabarker@users.noreply.github.com>
2024-01-23 19:20:26 +0000

    UPDATE REPLICATION.PY

--- epsilon-phi-core/src/python/epsilonPhi/research/option_attribution/Replication.py
+++ epsilon-phi-core/src/python/epsilonPhi/research/option_attribution/Replication.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import os, sys
+import statsmodels.api as sm
+
 
 
 ds = pd.read_csv('/Users/francisbarker/Desktop/SPX Options 1.csv')
@@ -34,7 +36,13 @@ At = sig.get(0)
 At_sq = np.power(At, 2)
 TA = At_sq * At_sq.columns.values
 
-mu = 0.5*At_sq.diff(axis=1)/TA.diff(axis=1)
+mu = (0.5 * At_sq.diff(axis=1)/TA.diff(axis=1)).dropna(axis=1)
+amh = 0.5 * (TA.columns.values[0:-1] + TA.columns.values[1:])
+mu.columns = amh
+mu[[At_sq.columns]] = np.nan
+mu_ = mu.sort_index(axis=1).interpolate(axis=1)
+mu_[mu_.columns[0]] = mu[mu.columns[0]]
+mu_ = mu_.get(At.columns)
 
 # Summary Stats for ATM Implied Vol Level (At)
 print(At.describe().reindex(['mean','std','min','max']))
@@ -44,3 +52,21 @@ print(np.log(At).diff().describe().reindex(['mean','std','min','max']) * np.arra
 
 # Summary Stats for Change in ATM Floating
 print(dsig.get(0).describe().reindex(['mean','std','min','max']) * np.array([252, np.sqrt(252), 252, 252]).reshape(-1, 1))
+
+print(mu_.describe().reindex(['mean','std','min','max']))
+
+# Check to see if We can Forecast Future Vol from Drift
+
+# X is the implied vol drift term
+# Y is the change in implied vol
+
+Xs = sm.add_constant(mu_)
+y = dsig.get(0) * 252
+
+for col in y.columns:
+  X_ = Xs[['const', col]]
+  y_ = y.get(col)
+
+  model = sm.OLS(y_, X_)
+  results = model.fit()
+
