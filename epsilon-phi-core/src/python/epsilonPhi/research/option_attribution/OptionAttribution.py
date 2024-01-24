bbaa73e1cf510151e9ec6686d9625c548fbc6681
Epsilon Phi <123123634+fabarker@users.noreply.github.com>
2024-01-24 15:34:44 +0000

    Update OptionAttribution.py

--- epsilon-phi-core/src/python/epsilonPhi/research/option_attribution/OptionAttribution.py
+++ epsilon-phi-core/src/python/epsilonPhi/research/option_attribution/OptionAttribution.py
@@ -7,10 +7,8 @@ class OptionAttributer(object):
     _DATA_PATH = '/Users/francisbarker/Desktop/SPX Options 1.csv'
     _DAYS_PER_YEAR = 365.25
     def __init__(self, start_date, end_date):
-
-        self._start_date = pd.to_datetime(start_date)
-        self._end_date = pd.to_datetime(end_date)
-        self._process_data()
+        self._process_data(start_date,
+                           end_date)
 
     @property
     def dates(self):
@@ -23,26 +21,93 @@ class OptionAttributer(object):
         return np.array(self._ds.index.get_level_values('T').unique()).reshape(1, -1)
     @property
     def spot(self):
-        return self._ds.get('s').reindex(self.dates).to_frame()
+        return self._ds.get('s').droplevel([1, 2]).drop_duplicates().reindex(self.dates).ffill().to_frame().copy()
     @property
     def sig(self):
-        return self._ds.get('sig').reindex(self.dates).to_frame()
+        return self._ds.get('sig').to_frame().unstack([1, 2]).get('sig').reindex(self.dates).sort_index(axis=1, level=1).copy()
     @property
     def dsig(self):
-        return
+        return self._ds.get('dsig').to_frame().unstack([1, 2]).get('dsig').reindex(self.dates).sort_index(axis=1, level=1).copy()
     @property
     def sig_sq(self):
-        return np.power(self.sig, 2)
+        return np.power(self.sig, 2).copy()
     @property
-    def A(self):
-        return
+    def atm_sig(self):
+        return self.sig.get(0).sort_index(axis=1).copy()
+    @property
+    def atm_dsig(self):
+        return self.dsig.get(0).sort_index(axis=1).copy()
 
-    def _process_data(self):
+    @property
+    def atm_sig_sq(self):
+        return np.power(self.sig, 2).get(0).copy()
+
+    def _process_data(self, start_date, end_date):
 
         _df = pd.read_csv('/Users/francisbarker/Desktop/SPX Options 1.csv')
         _df['date'] = pd.to_datetime(_df['date'])
-        _df = _df.set_index(['date','Z','T'], drop=True)
-        self._ds = _df.copy()
+        _df = _df.set_index(['date', 'Z', 'T'], drop=True)
+
+        keep_rows = np.logical_and(_df.index.get_level_values('date') >= pd.to_datetime(start_date),
+                                   _df.index.get_level_values('date') <= pd.to_datetime(end_date))
+
+        self._ds = _df.iloc[keep_rows].copy()
+        self._start_date = self._ds.index.get_level_values('date').min()
+        self._end_date = self._ds.index.get_level_values('date').max()
+
+    def get_mu(self, maturity=None):
+
+        if not hasattr(self, '_mu'):
+
+            Asq = self.atm_sig_sq
+            Asq_tau = Asq * Asq.columns.to_numpy().reshape(1, -1)
+
+            mu_ = ((1/2) * Asq.diff(axis=1) / Asq_tau.diff(axis=1)).dropna(axis=1)
+            X = 0.5 * Asq_tau.columns.values[0:-1] + 0.5 * Asq_tau.columns.values[1:]
+            mu_.columns = X
+
+            mu_[[Asq_tau.columns]] = np.nan
+            mu = mu_.sort_index(axis=1).interpolate(axis=1)
+            mu[mu.columns[0]] = mu_[mu_.columns[0]]
+            self._mu = mu.get(Asq.columns)
+
+        return self._mu.get(maturity, self._mu.copy())
+
+    def run_implied_vol_prediction_regressions(self):
+
+        # X is the implied vol drift term
+        # Y is the change in implied vol oevr the next time step
+        # Therefore these are prediction regressions over 1 day
+
+        mu = self.get_mu()
+        intercept = pd.DataFrame(np.ones(mu.shape[0]), columns=['constant'], index=mu.index)
+
+        regstats = [pd.DataFrame()]
+        for col in mu.columns:
+
+            mu_col_ = pd.concat((intercept, mu.get(col)), axis=1)
+            y_ = self.atm_dsig.get(col)
+
+            common_dates = np.intersect1d(mu_col_.index, y_.index)
+            reg = np.linalg.lstsq(mu_col_.loc[common_dates],
+                                  y_.loc[common_dates], rcond=None)
+
+            res = y_ - mu_col_ @ reg[0]
+            regstats.extend([pd.DataFrame([reg[0][0], reg[0][1],  1 - np.var(res) / np.var(y_)],
+                               index=['alpha', 'beta', 'rsq'], columns=[col])])
+        return pd.concat(regstats, axis=1)
+
+    def run_pca(self, start_date=None, end_date=None):
+
+        # X is the implied vol drift term
+        # Y is the change in implied vol oevr the next time step
+        # Therefore these are prediction regressions over 1 day
+
+        cov_mat = self.dsig[start_date:end_date].cov()
+        L, V = np.linalg.eig(cov_mat)
+        return pd.DataFrame(V, columns=L, index=self.dsig.columns).sort_index(axis=1, ascending=False)
+
 
 if __name__ == "__main__":
     self = OptionAttributer('31-Dec-1990', '31-Dec-2025')
+    self.A
