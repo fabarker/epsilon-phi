dd9df5b60710b0d4925c6a15251c6ad4ab6d5183
Epsilon Phi <123123634+fabarker@users.noreply.github.com>
2025-06-17 21:22:53 +0100

    polars iomplementation of basis mean reversion

--- epsilon-phi-core/src/python/epsilonPhi/core/dataModel/dataSources/futures/polars_impl.py
+++ epsilon-phi-core/src/python/epsilonPhi/core/dataModel/dataSources/futures/polars_impl.py
@@ -1,11 +1,16 @@
 from epsilonPhi.core.dataModel.dataSources.futures.Futures import Futures
+from epsilonPhi.core.utils.ExcelUtils import ExcelUtils
 import polars as pl
 import numpy as np
+import pandas as pd
+import os
 
 fut = Futures()
 
+
 def process_code(code):
 
+    print('processing {}'.format(code))
     df = fut.load_futures_prices_from_pickle(code).get(['PS']).reset_index()
     df = pl.from_pandas(df)
 
@@ -27,24 +32,30 @@ def process_code(code):
         (pl.col("date").dt.year() * 100 + pl.col("date").dt.week()).alias("week")
     )
 
+    week_codes = pd.Categorical(
+        pd.to_datetime(
+            df.select('date').to_pandas().values.flatten()
+        ).to_period('W')).codes
+
+    df = df.with_columns(
+        pl.Series('week', week_codes)
+    )
+
     # Weekly return per (symbol, week)
     weekly = (
         df
         .group_by(['week', 'symbol'])
         .agg([
-            ((pl.col('rets') + 1).product() - 1).alias('week'),
+            ((pl.col('rets') + 1).product() - 1).alias('rets'),
             pl.col('days').last().alias('days'),
             pl.col('date').last().alias('date')
         ])
         .sort(['week', 'symbol'])
     )
 
-    # Convert weeks to a list of unique sorted weeks
-    weeks = weekly.select('week').unique().sort('week')['week'].to_list()
-
     results = []
-    for i in range(len(weeks) - 1):
-        prev_w, next_w = weeks[i], weeks[i+1]
+    for prev_w in range(max(week_codes) - 1):
+        next_w = prev_w + 1
 
         prev_df = weekly.filter(pl.col('week') == prev_w)
         next_df = weekly.filter(
@@ -63,15 +74,18 @@ def process_code(code):
                    .limit(2)
         )
 
+        def get_eop(w):
+            return weekly.filter(pl.col('week') == w)['date'][-1]
+
         if candidates.height < 2:
-            results.append([np.nan] * 14 + [prev_w, next_w, code])
+            results.append([np.nan] * 12 + [get_eop(prev_w), get_eop(next_w), code])
             continue
 
         f0, f1 = candidates['symbol'][0], candidates['symbol'][1]
 
         # Get returns
         def get_ret(w, s):
-            return weekly.filter((pl.col('week') == w) & (pl.col('symbol') == s))['weekly_return'][0]
+            return weekly.filter((pl.col('week') == w) & (pl.col('symbol') == s))['rets'][0]
 
         f0_ret_0 = get_ret(prev_w, f0)
         f1_ret_0 = get_ret(prev_w, f1)
@@ -84,6 +98,7 @@ def process_code(code):
         # Extract days
         def get_days(w, s):
             return weekly.filter((pl.col('week') == w) & (pl.col('symbol') == s))['days'][0]
+
 
         res = [
             spread_0,
@@ -98,15 +113,15 @@ def process_code(code):
             get_days(next_w, f0),
             get_days(prev_w, f1),
             get_days(next_w, f1),
-            prev_w,
-            next_w,
+            get_eop(prev_w),
+            get_eop(next_w),
             code
         ]
         results.append(res)
 
-    return pl.DataFrame(
+    return pd.DataFrame(
         results,
-        schema=[
+        columns=[
             'spread_0', 'spread_t',
             'f0_ret_0', 'f1_ret_0',
             'f0_ret_1', 'f1_ret_1',
@@ -122,5 +137,12 @@ if __name__ == "__main__":
 
     codes = ['CL', 'ZO', 'ZM', 'ZL', 'LE', 'OJ', 'SB', 'GF', 'HO', 'KC', 'RB', 'GC', 'SI', 'HG', 'HE', 'ZS', 'ZR', 'CC', 'CT', 'ZW', 'NG', 'ZC']
 
+    res = {}
     for code in codes:
-        res = process_code(code)
+        res[code] = process_code(code).copy()
+
+ExcelUtils.dict_to_excel(
+    res,
+    os.path.join('futures_info/results_polars.xlsx'),
+    include_index=True,
+)
